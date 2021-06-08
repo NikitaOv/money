b98ea7689322270b9ec75b0b7696b3166c70e596
Nikita <kil8ler@gmail.com>
2021-06-08 17:07:43 +0600

    Have made adaptive

--- js/script.js
+++ js/script.js
@@ -1,17 +1,13 @@
 "use strict";
 
 const gamburger = document.querySelector('.gamburger'),
+      gamburgerSticks = document.querySelectorAll('.gamburger__stick'),
       header = document.querySelector('.header__links');
 
 gamburger.addEventListener('click', (e) => {
-    console.log('hello')
     header.classList.toggle('header__links_active');
-
-    header.addEventListener('click', (e) => {
-        console.log(1)
-        if (e.target != header) {
-            console.log(2)
-            header.classList.toggle('header__links_active');
-        }
-    })
+    
+    gamburgerSticks.forEach((elem) => {
+        elem.classList.toggle('gamburger__stick_close');
+    });
 })
